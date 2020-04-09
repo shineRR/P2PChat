61dbf38a871491769b8ef647ba93a5a630fec053
Ilya <barykoilyass@gmail.com>
2020-04-09 19:51:03 +0300

    Added supporting multiple users

--- src/client.go
+++ src/client.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net"
-	"strings"
+	"os"
+	"sync"
 )
 
 const Port = "8080"
 const BroadcastAdr = "192.168.0.255:8080"
+const CONNECT = "CONNECT"
+const RESPONSE = "RESPONSE"
+const RESPONSEUDP = "RESPONSEUDP"
+const PUBLIC  = "PUBLIC"
+const DISCONNECT = "DISCONNECT"
+
+type Message struct {
+	Kind      string //
+	Username  string //my username
+	IP        string //Ip address of my computer
+	MSG       string //message
+}
+
+var (
+	mutex = new(sync.Mutex)
+	myName string = ""
+	listConnections map[string]net.Conn = make(map[string]net.Conn)//list of users connections connected to mel
+	listIPs map[string]string = make(map[string]string)//list of users IPS connected to me
+)
+
+func createMessage(Kind string, Username string, IP string, Msg string) (msg *Message) {
+	msg = new(Message)
+	msg.Kind = Kind
+	msg.Username = Username
+	msg.IP = IP
+	msg.MSG = Msg
+	return
+}
 
 func getLocalIP() string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -23,21 +51,26 @@ func getLocalIP() string {
 	return localAddr.IP.String()
 }
 
-func createTCPConnection(IP string) {
-	fmt.Print(IP)
-	conn, err := net.Dial("tcp", IP)
+func createTCPConnection(IP string) (conn net.Conn) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", IP)
 	if err != nil {
-		// handle error
+		fmt.Println(err)
 	}
-	for {
-		enc:=json.NewEncoder(conn)
-		enc.Encode("privet")
+	conn, err = net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Println(err)
 	}
+	return
 }
 
 func listenTCPConnection(IP string) {
-	// Start listening to port 8080 for TCP connection
-	listener, err := net.Listen("tcp", IP + ":" + Port)
+	//log.Println("Server started \n")
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", IP + ":" + Port)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	listener, err := net.ListenTCP("tcp", tcpAddr)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -48,33 +81,66 @@ func listenTCPConnection(IP string) {
 		fmt.Println("Listener closed")
 	}()
 
-	for  {
+	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
 
-		go handleConnection(conn)
+		go receive(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
-
-	fmt.Println("Handling new connection...")
-	bufferBytes, err := bufio.NewReader(conn).ReadBytes('\n')
-	if err != nil {
-		log.Println("client left..")
+func receive(conn net.Conn) {
+	defer func() {
 		conn.Close()
+		fmt.Println("closed")
+	}()
 
-		return
+	dec := json.NewDecoder(conn)
+	msg := new(Message)
+	for {
+		if err := dec.Decode(msg); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		switch msg.Kind {
+		case CONNECT:
+			if !handleConnection(*msg, conn) {
+				return
+			}
+		case RESPONSE:
+			fmt.Println(msg.Username + " [" + msg.IP + "]" + " - " + msg.MSG)
+
+		case PUBLIC:
+			fmt.Println(msg.Username + " [" + msg.IP + "]" + " - " + msg.MSG)
+
+		case DISCONNECT:
+			//disconnect(*msg)
+			break
+		}
 	}
-	message := string(bufferBytes)
-	clientAddr := conn.RemoteAddr().String()
-	fmt.Println(message + " from " + clientAddr + "\n")
+}
 
-	newmessage := strings.ToUpper(message)
-	conn.Write([]byte(newmessage + "\n"))
+func disconnect(msg Message) {
+
+}
+
+func handleConnection(msg Message, conn net.Conn) bool {
+	mutex.Lock()
+	listConnections[msg.Username] = conn
+	listIPs[msg.Username] = msg.IP
+	mutex.Unlock()
+	return true
+}
+
+func (msg *Message) sendMessage() {
+	for _,peerConnection := range listConnections{
+		enc:=json.NewEncoder(peerConnection)
+		enc.Encode(msg)
+	}
 }
 
 func listenUDP() {
@@ -89,12 +155,19 @@ func listenUDP() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s sent this: %s\n", addr, buf[:n])
+	fmt.Printf("%s connected and ready to chat :)\n", buf[:n])
 
-	go createTCPConnection(addr.String())
+	receivedMsg := createMessage(CONNECT, string(buf[:n]), addr.String(), "123")
+	conn := createTCPConnection(addr.String())
+	handleConnection(*receivedMsg, conn)
+	enc:= json.NewEncoder(conn)
+	introMessage := createMessage(CONNECT, myName, getLocalIP(), "Response for UDP packet\n")
+	enc.Encode(introMessage)
+	go listenUDP()
+	go receive(conn)
 }
 
-func sendUDP(name string) {
+func (MSG *Message) sendUDP() {
 	listenAddr, err := net.ResolveUDPAddr("udp4", ":" + Port)
 	if err != nil {
 		panic(err)
@@ -110,7 +183,7 @@ func sendUDP(name string) {
 		panic(err)
 	}
 
-	_, err = list.WriteTo([]byte(name + " Connected :)"), addr)
+	_, err = list.WriteTo([]byte(MSG.Username), addr)
 	if err != nil {
 		panic(err)
 	}
@@ -120,13 +193,38 @@ func introduceMyself() {
 	fmt.Print("Enter your name: ")
 	var name string
 	fmt.Scanln(&name)
-	sendUDP(name)
+	fmt.Println("Server - Welcome to the chat " + name + " :)")
+	msg := createMessage("CONNECT", name, getLocalIP(), "Hello, my friend\n")
+	myName = name
+	msg.sendUDP()
+}
+
+func server() {
+	go listenTCPConnection(getLocalIP())
+}
+
+func userInput() {
+	msg := new(Message)
+	for {
+		var message string
+		//fmt.Print("Message: ")x
+		fmt.Scanln(&message)
+		//fmt.Println("Me - ", message)
+		if message == "disconnect"{
+			msg = createMessage(DISCONNECT, myName, getLocalIP(),"Disconnected")
+			msg.sendMessage()
+			break
+		} else if len(message) > 1 {
+			msg = createMessage(PUBLIC, myName, getLocalIP(), message)
+			msg.sendMessage()
+		}
+	}
+	os.Exit(1)
 }
 
 func main() {
 	introduceMyself()
-	go listenTCPConnection(getLocalIP())
-	for {
-		listenUDP()
-	}
+	go listenUDP()
+	go server()
+	userInput()
 }
